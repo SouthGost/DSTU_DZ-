14c81f13b2aaef7eade3e7af83c4bac27b9d96ec
Arnovec <53020329+Arnovec@users.noreply.github.com>
2022-03-05 23:38:31 +0300

    Update Lab.java

--- 3.3/Romonenko/ZashLab2/Lab.java
+++ 3.3/Romonenko/ZashLab2/Lab.java
@@ -6,76 +6,174 @@ import java.util.Scanner;
 
 public class Lab  {
     public static void main(String[] args) {
-
-    }
-}
-
-abstract class Action{
-    public final String name;
-
-    public Action(String name){
-        this.name = name;
-    }
-
-    abstract public int make();
-}
-
-class Menu extends Action{
-    private static Scanner in = new Scanner(System.in);
-    private final ArrayList<Action> actions;
-    private final boolean isRepeat;
-
-    public Menu(ArrayList<Action> actions, String name, boolean isRepeat){
-        super(name);
-        this.actions = actions;
-        this.isRepeat = isRepeat;
-    }
-
-    @Override
-    public int make(){
-        int i = 1;
-
-        for(var action: actions){
-            System.out.println(i + ") " + action.name);
-            i++;
-        }
-        int actionNumber = in.nextInt();
-
-        if(actionNumber>0 && actionNumber <i-1){
-            actions.get(actionNumber).make();
-        } else if(actionNumber == 0){
-
-            return 0;
-        } else {
-            System.out.println("Непонятное действие");
-            this.make();
-
-            return 0;
-        }
-
-        if(isRepeat){
-            this.make();
-        }
-
-        return 0;
+        CompSystem cs = new CompSystem(new String[]{"Vasya","Petya","Ura"});
+        cs.start();
     }
 }
 
 class CompSystem{
+    private static Scanner in = new Scanner(System.in);
     private String[] users;
     private int[] datas;
-    int[][] permissions;
-    Menu compMenu;
+    private int[][] permissions;
+    private int currentUser = -1;
+
+
 
     public CompSystem(String[] users){
         this.users = users;
         Random rand = new Random();
-        datas = new int[(rand.nextInt()%10)+5];//длинна данных
+        datas = new int[Math.abs(rand.nextInt()%10)+5];//длинна данных
 
         for(int i = 0; i < datas.length; i++){
-            datas[i] = rand.nextInt();
+            datas[i] = rand.nextInt()%100;
         }
 
+        permissions = new int [users.length][];
+        permissions[0] = new int[datas.length];
+        for (int i = 0;i<datas.length;i++){
+            permissions[0][i] = 7;
+        }
 
+        for (int i = 1;i<users.length;i++){
+            permissions[i] = new int[datas.length];
+            for (int j = 0;j<datas.length;j++){
+                permissions[i][j] = Math.abs(rand.nextInt()%8);
+            }
+        }
+
+    }
+
+    private boolean checkAccess(int dataId,int pos){
+        if( (permissions[currentUser][dataId]/(int)Math.pow(2,pos-1)) % 2  == 1){
+            return true;
+        }
+        return false;
+    }
+
+    private void menu(){
+        System.out.println("Жду ваших указаний:");
+        String action = in.nextLine();
+
+        int dataId;
+        switch (action){
+            case "read":
+                System.out.println("Над каким объектом произвести операцию:");
+                dataId = in.nextInt();
+                in.nextLine();
+                if(checkAccess(dataId,3)){
+                    System.out.println(datas[dataId]);
+                } else {
+                    System.out.println("Нет доступа");
+                }
+
+                break;
+            case "write":
+                System.out.println("Над каким объектом произвести операцию:");
+                dataId = in.nextInt();
+                in.nextLine();
+                if(checkAccess(dataId,2)){
+                    System.out.println("Введите новое значение:");
+                    int newVal = in.nextInt();
+                    in.nextLine();
+                    datas[dataId] = newVal;
+                } else {
+                    System.out.println("Нет доступа");
+                }
+
+                break;
+            case "grant":
+                System.out.println("Право на какой объект передается:");
+                dataId = in.nextInt();
+                in.nextLine();
+                if(checkAccess(dataId,1)){
+                    System.out.println("Какое право передается:");
+                    action = in.nextLine();
+                    int changePos = 0;
+                    switch (action){
+                        case "read":
+                            changePos = 3;
+                            break;
+                        case "write":
+                            changePos = 2;
+                            break;
+                        case "grant":
+                            changePos = 1;
+                            break;
+                    }
+                    if(changePos != 0){
+                        System.out.println("Какому пользователю передается право:");
+                        String whatUser = in.nextLine();
+                        boolean isFind = false;
+                        int userId = 0;
+                        for(var u:users){
+                            if(u.equals(whatUser)){
+                                isFind = true;
+                                break;
+                            }
+                            userId++;
+                        }
+                        if(isFind){
+                            if((permissions[userId][dataId]/(int)Math.pow(2,changePos-1)) % 2  != 1){
+                                permissions[userId][dataId] += Math.pow(2,changePos-1);
+                            }
+                        } else {
+                            System.out.println("Неизвестный пользователь");
+                        }
+                    } else{
+                        System.out.println("Неизвестное право");
+                    }
+
+                } else {
+                    System.out.println("Нет доступа");
+                }
+
+                break;
+            case "quit":
+
+                return;
+            default:
+                System.out.println("Непонятное действие");
+        }
+
+        this.menu();
+
+        return;
+    }
+
+    public void start(){
+        System.out.println("Введите имя пользователя:");
+        String userName = in.nextLine();
+        boolean isFind = false;
+        int i = 0;
+        for(var u:users){
+            if(u.equals(userName)){
+                isFind = true;
+                break;
+            }
+            i++;
+        }
+        if(isFind){
+            currentUser = i;
+            for(int j = 0; j< datas.length; j++){
+                System.out.print("Объект " + j + ": ");
+                if(checkAccess(j,3)){
+                    System.out.print("Чтение ");
+                }
+                if(checkAccess(j,2)){
+                    System.out.print("Запись ");
+                }
+                if(checkAccess(j,1)){
+                    System.out.print("Передача прав ");
+                }
+                System.out.println();
+            }
+            menu();
+            currentUser = -1;
+        } else {
+            System.out.println("Нет такого пользователя");
+        }
+
+        start();
     }
 }
